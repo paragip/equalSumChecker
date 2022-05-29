3106a1ff78dd2d15281ba1cbf121c1749abed3e5
paragip <pal.paragi@gmail.com>
2022-05-29 20:28:20 +0200

    check if sum of parameter 1 and 2 is equal with parameter 3 or not

---
+++ src/equalSumChecker.java
@@ -0,0 +1,9 @@
+public class equalSumChecker {
+    public static void main (String[] args) {
+        System.out.println(hasEqualSum(2,3,66));
+    }
+
+    public static boolean hasEqualSum(int param1, int param2, int param3){
+        return param1 + param2 == param3; // if statement egyszerűsítve
+    }
+}
